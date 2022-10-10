4378c75725d482275b48cd789b7a2a430530b459
ssuruthim <107911232+ssuruthim@users.noreply.github.com>
2022-10-10 11:49:27 +0530

    Add files via upload

---
+++ email.js
@@ -0,0 +1,7 @@
+var email = prompt("enter your mailid");
+var l = email.length;
+for (i=0;i<l;i++){
+    if(email[i]==1||email[i]==3||email[i]==5||email[i]==7||email[i]==9)
+{
+    console.log(email[i]);
+}}
